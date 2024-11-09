dc0b9e1b3e796d326f5c500a2770b7766bccc3fc
Michael Woolnough <michael.woolnough@gmail.com>
2024-11-09 22:21:54 +0000

    add option to enable keeping of export in specified file (and remove experimental sorting option)

--- cmd/jspacker/main.go
+++ cmd/jspacker/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"sort"
 
 	"vimagination.zapto.org/javascript"
 	"vimagination.zapto.org/jspacker"
@@ -36,7 +35,7 @@ func run() error {
 	var (
 		output, base               string
 		filesTodo                  Inputs
-		plugin, noExports, reorder bool
+		plugin, noExports, exports bool
 		err                        error
 	)
 
@@ -45,7 +44,7 @@ func run() error {
 	flag.StringVar(&base, "b", "", "js base dir")
 	flag.BoolVar(&plugin, "p", false, "export file as plugin")
 	flag.BoolVar(&noExports, "n", false, "no exports")
-	flag.BoolVar(&reorder, "x", false, "experimental script re-ordering to enable better minification (BE CAREFUL)")
+	flag.BoolVar(&exports, "e", false, "keep primary file exports")
 	flag.Parse()
 
 	if plugin && len(filesTodo) != 1 {
@@ -69,7 +68,7 @@ func run() error {
 		return fmt.Errorf("error getting absolute path for base: %w", err)
 	}
 
-	var s *javascript.Script
+	var s *javascript.Module
 
 	if plugin {
 		f, err := os.Open(filepath.Join(base, filepath.FromSlash(filesTodo[0])))
@@ -100,6 +99,10 @@ func run() error {
 			args = append(args, jspacker.NoExports)
 		}
 
+		if exports {
+			args = append(args, jspacker.PrimaryExports)
+		}
+
 		for _, f := range filesTodo {
 			args = append(args, jspacker.File(f))
 		}
@@ -109,12 +112,8 @@ func run() error {
 		}
 	}
 
-	for len(s.StatementList) > 0 && s.StatementList[0].Declaration == nil && s.StatementList[0].Statement == nil {
-		s.StatementList = s.StatementList[1:]
-	}
-
-	if reorder {
-		sort.Stable(statementSorter(s.StatementList[1:]))
+	for len(s.ModuleListItems) > 0 && s.ModuleListItems[0].ImportDeclaration == nil && s.ModuleListItems[0].ExportDeclaration == nil && s.ModuleListItems[0].StatementListItem == nil {
+		s.ModuleListItems = s.ModuleListItems[1:]
 	}
 
 	var of *os.File
@@ -132,49 +131,4 @@ func run() error {
 	}
 
 	return nil
-}
-
-type statementSorter []javascript.StatementListItem
-
-func (s statementSorter) Len() int {
-	return len(s)
-}
-
-func (s statementSorter) Less(i, j int) bool {
-	if scoreA, scoreB := score(&s[i]), score(&s[j]); scoreA != scoreB {
-		return scoreA > scoreB
-	}
-
-	return i < j
-}
-
-func (s statementSorter) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func score(sli *javascript.StatementListItem) uint8 {
-	if sli.Statement != nil {
-		if sli.Statement.ExpressionStatement != nil && len(sli.Statement.ExpressionStatement.Expressions) == 1 && sli.Statement.ExpressionStatement.Expressions[0].ConditionalExpression != nil && sli.Statement.ExpressionStatement.Expressions[0].AssignmentOperator == javascript.AssignmentNone {
-			if ce, ok := javascript.UnwrapConditional(sli.Statement.ExpressionStatement.Expressions[0].ConditionalExpression).(*javascript.CallExpression); ok {
-				if ce.MemberExpression != nil && ce.MemberExpression.MemberExpression != nil && ce.MemberExpression.MemberExpression.PrimaryExpression != nil && ce.MemberExpression.MemberExpression.PrimaryExpression.IdentifierReference != nil && ce.MemberExpression.MemberExpression.PrimaryExpression.IdentifierReference.Data == "customElements" && ce.MemberExpression.IdentifierName != nil && ce.MemberExpression.IdentifierName.Data == "define" {
-					return 4
-				}
-			}
-		} else if sli.Statement.VariableStatement != nil {
-			return 2
-		}
-	} else if sli.Declaration != nil {
-		if sli.Declaration.ClassDeclaration != nil {
-			return 6
-		} else if sli.Declaration.FunctionDeclaration != nil {
-			return 5
-		} else if sli.Declaration.LexicalDeclaration != nil {
-			if sli.Declaration.LexicalDeclaration.LetOrConst == javascript.Let {
-				return 3
-			}
-			return 1
-		}
-	}
-
-	return 0
 }
