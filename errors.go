aaee44ced16d485a570fa1da5553fe6b1a99fe73
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-17 19:47:48 +0000

    sorted errors

--- errors.go
+++ errors.go
@@ -5,6 +5,6 @@ import "errors"
 // Errors.
 var (
 	ErrInvalidExport = errors.New("invalid export")
-	ErrNoFiles       = errors.New("no files")
 	ErrInvalidURL    = errors.New("added files must be absolute URLs")
+	ErrNoFiles       = errors.New("no files")
 )
