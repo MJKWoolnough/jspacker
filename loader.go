93240fbf15ec61afb03793a06837e3c717d501b5
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-19 20:55:56 +0000

    split out file processing code into own func

--- loader.go
+++ loader.go
@@ -16,26 +16,9 @@ func jToken(data string) *javascript.Token {
 }
 
 func (c *config) makeLoader() error {
-	obs := make([]javascript.LexicalBinding, 0, len(c.filesDone))
-
-	for _, file := range sortedMap(c.filesDone) {
-		if !file.requireNamespace && c.bare && !c.parseDynamic && !c.dynamicRequirement {
-			continue
-		}
-
-		fields := make([]javascript.PropertyDefinition, 0, len(file.exports))
-
-		for binding := range sortedMap(file.exports) {
-			b := file.resolveExport(binding)
-
-			if b == nil {
-				return fmt.Errorf("error resolving export %s (%s): %w", binding, file.url, ErrInvalidExport)
-			}
-
-			fields = append(fields, makeGetter(binding, b.Token))
-		}
-
-		obs = append(obs, wrapNameSpaceFields(file.prefix, fields))
+	obs, err := c.processFiles()
+	if err != nil {
+		return err
 	}
 
 	if len(obs) == 0 {
@@ -57,6 +40,32 @@ func (c *config) makeLoader() error {
 	c.moduleItems = slices.Insert(c.moduleItems, 1, wrapImports(imports))
 
 	return nil
+}
+
+func (c *config) processFiles() ([]javascript.LexicalBinding, error) {
+	obs := make([]javascript.LexicalBinding, 0, len(c.filesDone))
+
+	for _, file := range sortedMap(c.filesDone) {
+		if !file.requireNamespace && c.bare && !c.parseDynamic && !c.dynamicRequirement {
+			continue
+		}
+
+		fields := make([]javascript.PropertyDefinition, 0, len(file.exports))
+
+		for binding := range sortedMap(file.exports) {
+			b := file.resolveExport(binding)
+
+			if b == nil {
+				return nil, fmt.Errorf("error resolving export %s (%s): %w", binding, file.url, ErrInvalidExport)
+			}
+
+			fields = append(fields, makeGetter(binding, b.Token))
+		}
+
+		obs = append(obs, wrapNameSpaceFields(file.prefix, fields))
+	}
+
+	return obs, nil
 }
 
 func sortedMap[K cmp.Ordered, V any](m map[K]V) iter.Seq2[K, V] {
