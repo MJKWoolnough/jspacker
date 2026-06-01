3cfb7830973a2fff29f42e5a7a082f59beb18553
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-01 07:07:19 +0100

    OSLoad should be in options

--- jspacker.go
+++ jspacker.go
@@ -3,15 +3,11 @@ package jspacker
 
 import (
 	"fmt"
-	"iter"
-	"net/url"
 	"os"
-	"path/filepath"
 	"slices"
 	"strings"
 
 	"vimagination.zapto.org/javascript"
-	"vimagination.zapto.org/parser"
 )
 
 type config struct {
@@ -26,110 +22,6 @@ type config struct {
 	exportAllFrom [][2]*dependency
 	moduleItems   []javascript.ModuleItem
 	dependency
-}
-
-const (
-	jsSuffix = ".js"
-	tsSuffix = ".ts"
-)
-
-func loadFns(base, urlPath string, ts *bool) iter.Seq[func() (*os.File, error)] {
-	return func(yield func(func() (*os.File, error)) bool) {
-		for _, fn := range [...]func() (*os.File, error){
-			func() (*os.File, error) { // Assume that any TS file will be more up-to-date by default
-				if strings.HasSuffix(urlPath, jsSuffix) {
-					*ts = true
-
-					return os.Open(filepath.Join(base, filepath.FromSlash(urlPath[:len(urlPath)-3]+tsSuffix)))
-				}
-
-				return nil, nil
-			},
-			func() (*os.File, error) { // Normal
-				f, err := os.Open(filepath.Join(base, filepath.FromSlash(urlPath)))
-				if err == nil {
-					*ts = strings.HasSuffix(urlPath, tsSuffix)
-				}
-
-				return f, err
-			},
-			func() (*os.File, error) { // As URL
-				if u, err := url.Parse(urlPath); err == nil && u.Path != urlPath {
-					f, err := os.Open(filepath.Join(base, filepath.FromSlash(u.Path)))
-					if err == nil {
-						*ts = strings.HasSuffix(urlPath, tsSuffix)
-					}
-
-					return f, err
-				}
-
-				return nil, nil
-			},
-			func() (*os.File, error) { // Add TS extension
-				if !strings.HasSuffix(urlPath, tsSuffix) {
-					*ts = true
-
-					return os.Open(filepath.Join(base, filepath.FromSlash(urlPath+tsSuffix)))
-				}
-
-				return nil, nil
-			},
-			func() (*os.File, error) { // Add JS extension
-				if !strings.HasSuffix(urlPath, jsSuffix) {
-					return os.Open(filepath.Join(base, filepath.FromSlash(urlPath+jsSuffix)))
-				}
-
-				return nil, nil
-			},
-		} {
-			if !yield(fn) {
-				return
-			}
-		}
-	}
-}
-
-// OSLoad is the default loader for Package, with the base set to CWD.
-func OSLoad(base string) func(string) (*javascript.Module, error) {
-	return func(urlPath string) (*javascript.Module, error) {
-		var (
-			f   *os.File
-			err error
-		)
-		ts := strings.HasSuffix(base, tsSuffix)
-
-		for loader := range loadFns(base, urlPath, &ts) {
-			fb, errr := loader()
-			if fb != nil {
-				f = fb
-
-				break
-			} else if err == nil {
-				err = errr
-			}
-		}
-
-		if f == nil {
-			return nil, fmt.Errorf("error opening file (%s): %w", urlPath, err)
-		}
-
-		defer f.Close()
-
-		rt := parser.NewReaderTokeniser(f)
-
-		var tks javascript.Tokeniser = &rt
-
-		if ts {
-			tks = javascript.AsTypescript(&rt)
-		}
-
-		m, err := javascript.ParseModule(tks)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing file (%s): %w", urlPath, err)
-		}
-
-		return m, nil
-	}
 }
 
 // Package packages up multiple JavaScript modules into a single file, renaming
